0352e53ef25d31d6b0096f273de5c605da5b7080
Mariana Marrão <marimarrao0115@gmail.com>
2026-02-04 12:36:23 -0300

    Remover a constraint default do campo do email (#4)
    * feat: add initial database script
    
    * refactor: replace NOT NULL with DEFAULT constraints
    
    * refactor: removing the default constraint of the email

---
+++ script_sql.sql
@@ -0,0 +1,30 @@
+CREATE TABLE student (
+	enrollment SERIAL PRIMARY KEY,
+	name VARCHAR(150) DEFAULT 'Não Informado',
+	email VARCHAR(345) UNIQUE,
+	password VARCHAR(255) DEFAULT 'Não Informado',
+	status INT DEFAULT 1,
+	cpf VARCHAR(11) NOT NULL UNIQUE CHECK (cpf ~ '^[0-9]{11}$')
+);
+
+CREATE TABLE subject (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL UNIQUE,
+	teacher_name VARCHAR(150) NOT NULL,
+	teacher_user VARCHAR(50) NOT NULL UNIQUE, 
+	teacher_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE student_subject (
+	student_enrollment INT REFERENCES student(enrollment) NOT NULL,
+	subject_id INT REFERENCES subject(id) NOT NULL,
+	PRIMARY KEY (student_enrollment, subject_id),
+	grade1 NUMERIC(4,2) CHECK (grade1 BETWEEN 0 AND 10),
+	grade2 NUMERIC(4,2) CHECK (grade2 BETWEEN 0 AND 10),
+	obs TEXT
+);
+
+CREATE TABLE admin (
+	document VARCHAR(20) PRIMARY KEY,
+	password VARCHAR(255) NOT NULL
+);
