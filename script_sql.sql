22a77c08b057903143311a77dd03222b043d890d
marianamarrao <marimarrao0115@gmail.com>
2026-02-10 08:17:36 -0300

    refactor: update database schema and relationships

--- script_sql.sql
+++ script_sql.sql
@@ -1,32 +1,59 @@
+CREATE TABLE school_class (
+	id SERIAL PRIMARY KEY,
+	school_year VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE teacher (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150) NOT NULL,
+	email VARCHAR(355) NOT NULL UNIQUE,
+	username VARCHAR(50) NOT NULL UNIQUE, 
+	password VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE student (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(150) DEFAULT 'não informado',
 	email VARCHAR(345) UNIQUE,
 	password VARCHAR(255) DEFAULT 'não informado',
 	status INT DEFAULT 1,
-	cpf VARCHAR(11) NOT NULL UNIQUE CHECK (cpf ~ '^[0-9]{11}$')
+	cpf VARCHAR(11) NOT NULL UNIQUE CHECK (cpf ~ '^[0-9]{11}$'),
+	id_school_class INT REFERENCES school_class(id)
 );
 
 CREATE TABLE subject (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL UNIQUE,
-	teacher_name VARCHAR(150) NOT NULL,
-	teacher_user VARCHAR(50) NOT NULL UNIQUE, 
-	teacher_password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE student_subject (
-	id SERIAL PRIMARY KEY,
-	student_id INT NOT NULL REFERENCES student(id) ON DELETE CASCADE,
-	subject_id INT NOT NULL REFERENCES subject(id) ON DELETE CASCADE,
-	CONSTRAINT uk_student_subject UNIQUE (student_id, subject_id),
-	grade1 NUMERIC(4,2) CHECK (grade1 BETWEEN 0 AND 10),
-	grade2 NUMERIC(4,2) CHECK (grade2 BETWEEN 0 AND 10),
-	obs TEXT
+	deadline DATE NOT NULL
 );
 
 CREATE TABLE admin (
 	id SERIAL PRIMARY KEY,
 	document VARCHAR(11),
+	email VARCHAR(355) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE school_class_teacher(
+	id SERIAL PRIMARY KEY,
+	id_school_class INT REFERENCES school_class(id),
+	id_teacher INT REFERENCES teacher(id),
+	CONSTRAINT uk_school_class_teacher UNIQUE (id_school_class, id_teacher)
+);
+
+CREATE TABLE subject_teacher(
+	id SERIAL PRIMARY KEY, 
+	id_subject INT REFERENCES subject(id) NOT NULL,
+	id_teacher INT REFERENCES teacher(id) NOT NULL,
+	CONSTRAINT uk_subject_teacher UNIQUE (id_subject, id_teacher)
+);
+
+CREATE TABLE student_subject (
+    id SERIAL PRIMARY KEY,
+    id_student INT REFERENCES student(id) NOT NULL,
+    id_subject INT REFERENCES subject(id) NOT NULL,
+    grade1 NUMERIC(4,2) CHECK (grade1 BETWEEN 0 AND 10),
+    grade2 NUMERIC(4,2) CHECK (grade2 BETWEEN 0 AND 10),
+    obs TEXT,
+    CONSTRAINT uk_student_subject UNIQUE (id_student, id_subject)
 );
